2e32927fc5ed9ccf738cc4749dee9400337d8045
Kamel Faour <10121641@mu.edu.lb>
2024-08-27 22:00:43 +0300

    Django Admin/Pages,urls,slugs all updated

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Here we deal with Database and Tables
+
+# Here is a table called Post with its attriutes
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
